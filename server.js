658beb7566ff08e1325c80016b13c564eb6fd928
Sherif <127263385+sizif-22@users.noreply.github.com>
2025-03-07 06:01:25 +0200

    fixing some bugs

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const app = require("./app");
 const { TIMEZONE } = require("./utils/dateUtils");
 const { loadUnsentMessages } = require("./services/messageService");
 
-// const PORT = process.env.PORT || 3000;
-
 console.log("env vars: ", process.env.PORT);
 console.log("env vars: ", process.env.PORT);
 console.log("env vars: ", process.env.PORT);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+// const PORT = 3000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
